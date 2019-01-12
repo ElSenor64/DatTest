496083dce4035619b083be937aec91db928726fa
ElSenor64 <sillynoodle36@gmail.com>
2019-01-12 10:36:43 -0700

    Wat Da Hick!?!

---
+++ Terminator.java
@@ -0,0 +1,18 @@
+public class Terminator {
+    public static void main(String[] args) {
+        String x = "Exterminate!!!";
+        System.out.println(x);
+        //no
+        //no
+        //ho
+        //boogie woogie
+        //ho
+    }
+}
+
+
+
+
+
+
+
