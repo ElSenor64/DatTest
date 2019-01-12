6c6bd7fd7cb8f35a77e0eafd3a26aa6ed25ad1c7
ElSenor64 <sillynoodle36@gmail.com>
2019-01-12 15:41:19 -0700

    Details for the second major project uploaded.

---
+++ PROJECT2.java
@@ -0,0 +1,17 @@
+/*
+Prototypes
+
+BALL
+One Roller
+Double Decker Rocky
+
+HATCH PANEL
+Spatula
+
+PROJECTS
+Cargo prototypes - Gavin
+Hatch prototypes - Jacob
+Drive - Rygar
+Frogy Catapult (Lift) - Josh
+
+*/
